72b33134347af8aae5be74d9f28f38f3254786f1
Jay Ko <jko@bscgp.ca>
2026-02-16 23:56:48 -0800

    fix: install autoprefixer and fix folder nesting

--- src/app/app/feature/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function FeaturesPage() {
-  return (
-    <div className="bg-[#0b101a] min-h-screen text-white p-20 text-center">
-      <h1 className="bowl-font text-5xl mb-10 text-[#e5ff4d]">FEATURES</h1>
-      <p className="inter-font text-gray-400">상세한 서비스 특징을 여기에 작성하세요.</p>
-    </div>
-  );
-}
