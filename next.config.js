7e986a66b9be85eb9451d0c665bb4d29b2bea02f
Muntalk <muntalkofficial@gmail.com>
2026-03-29 21:59:50 -0700

    fix: remove inline style to fix build error

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
   images: { unoptimized: true },
-  // 빌드 캐시가 저장되는 위치를 아예 랜덤한 새 폴더로 지정합니다.
-  distDir: 'build_output_' + Date.now(), 
-  webpack: (config) => {
-    config.cache = false; // 기존 캐시를 아예 사용하지 않음
-    return config;
-  },
 };
 
 module.exports = nextConfig;
